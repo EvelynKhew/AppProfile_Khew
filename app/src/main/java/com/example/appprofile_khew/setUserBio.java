82935efe0f9df7fd600b4fb27413dec1f83a8e1a
evelyn <evelyn@iastate.edu>
2021-09-10 23:18:51 -0700

    Created activities

---
+++ app/src/main/java/com/example/appprofile_khew/setUserBio.java
@@ -0,0 +1,14 @@
+package com.example.appprofile_khew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class setUserBio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_user_bio);
+    }
+}
---
+++ app/src/main/java/com/example/appprofile_khew/setUserEmail.java
@@ -0,0 +1,14 @@
+package com.example.appprofile_khew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class setUserEmail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_user_email);
+    }
+}
